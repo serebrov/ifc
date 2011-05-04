2fab132cf746fa82c0afdfaf153ed7a46e207a69
Boris Serebrov <seb.goo@gmail.com>
2011-05-04 16:12:24 +0300

    add ConSpect *.prn file convert tool script

---
+++ tools/prn-convert/conv_prn.py
@@ -0,0 +1,27 @@
+fileName = '100inp.prn'
+# fileNameOut = '100inp.out'
+
+f = open(fileName,'rt')
+values = []
+for line in f.readlines():
+    for val in line.split():
+        if (val):
+            values.append(val)
+
+count = int(values[0])
+lines = []
+#y = values[-count:]
+lines.append(values[-count:])
+#x = values[-count*2:-count]
+lines.append(values[-count*2:-count])
+#special = values[:-count*2]
+lines.append(values[:-count*2])
+
+for i in range(count):
+    out = ""
+    for line in lines:
+        if (len(line) > i):
+            out += line[i] + ' '
+        else:
+            out += ' '
+    print out
---
+++ tools/prn-convert/readme.txt
@@ -0,0 +1,3 @@
+Input file - 100inp.prn (ConSpect data file)
+
+Run: python conv_prn.py > out.txt
